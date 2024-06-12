5f5113c2282013c5f19faa7d66636d783014b83a
Jawad BOUGHALEM <jboughalem@dailybiz.com>
2024-06-12 14:30:38 +0200

    Mise à jour App.js

--- App.js
+++ App.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-<<<<<<< HEAD
 import { Ionicons, MaterialIcons } from '@expo/vector-icons';
 import { TouchableOpacity, View, StyleSheet } from 'react-native';
-=======
-import { createStackNavigator } from '@react-navigation/stack';
-import { Ionicons, MaterialIcons } from '@expo/vector-icons';
-import { TouchableOpacity, StyleSheet } from 'react-native';
->>>>>>> master
 import { BlurView } from 'expo-blur';
 
 import Annonces from './screens/Annonces';
@@ -17,26 +11,9 @@ import Photos from './screens/Photos';
 import Conseils from './screens/Conseils';
 import Profil from './screens/Profil';
 import CameraPreview from './screens/CameraPreview';
-<<<<<<< HEAD
 import Formulaire from './screens/Formulaire';
 
 const Tab = createBottomTabNavigator();
-=======
-import CreatePost from './screens/CreatePost';
-
-const Tab = createBottomTabNavigator();
-const Stack = createStackNavigator();
-
-function PhotosStack() {
-  return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Photos" component={Photos} />
-      <Stack.Screen name="CameraPreview" component={CameraPreview} />
-      <Stack.Screen name="CreatePost" component={CreatePost} />
-    </Stack.Navigator>
-  );
-}
->>>>>>> master
 
 export default function App() {
   return (
@@ -86,13 +63,8 @@ export default function App() {
           }}
         />
         <Tab.Screen
-<<<<<<< HEAD
           name="Photos"
           component={Photos}
-=======
-          name="PhotosStack"
-          component={PhotosStack}
->>>>>>> master
           options={{
             tabBarLabel: () => null,
             tabBarIcon: ({ color, focused }) => null,
