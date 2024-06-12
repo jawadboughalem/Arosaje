df3693e65f105cf72787b2eed28ef674f8c9eda8
Jawad BOUGHALEM <jboughalem@dailybiz.com>
2024-06-12 14:33:58 +0200

    CameraPreview

--- screens/CameraPreview.js
+++ screens/CameraPreview.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { useNavigation, useRoute } from '@react-navigation/native';
+import { View, Image, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
-export default function CameraPreview() {
-  const navigation = useNavigation();
-  const route = useRoute();
+const CameraPreview = ({ route }) => {
   const { photo } = route.params;
+  const navigation = useNavigation();
 
   return (
     <View style={styles.container}>
       <Image source={{ uri: photo }} style={styles.image} />
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => navigation.navigate('Formulaire', { photo })}
-        >
-          <Text style={styles.text}>Create Announcement</Text>
-        </TouchableOpacity>
-      </View>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate('CreatePost', { photo })}
+      >
+        <Text style={styles.buttonText}>Créer un post</Text>
+      </TouchableOpacity>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -34,23 +31,17 @@ const styles = StyleSheet.create({
     height: '60%',
     borderRadius: 15,
   },
-  buttonContainer: {
-    position: 'absolute',
-    bottom: 50,
-    left: 0,
-    right: 0,
-    flexDirection: 'row',
-    justifyContent: 'center',
-  },
   button: {
-    padding: 10,
-    backgroundColor: '#5DB075',
-    borderRadius: 5,
     marginTop: 20,
+    backgroundColor: '#5DB075',
+    padding: 15,
+    borderRadius: 10,
   },
-  text: {
+  buttonText: {
     color: 'white',
     fontSize: 18,
     fontWeight: 'bold',
   },
 });
+
+export default CameraPreview;
