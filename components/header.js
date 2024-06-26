26a3c609540f9a038e778e7946ca261defec5f45
Jawad BOUGHALEM <jboughalem@dailybiz.com>
2024-06-26 15:52:18 +0200

    MAJ

--- components/header.js
+++ components/header.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { BlurView } from 'expo-blur';
+import { LinearGradient } from 'expo-linear-gradient';
 
 const Header = ({ title }) => {
   return (
     <View style={styles.headerContainer}>
-      <BlurView intensity={95} tint="light" style={styles.header}>
+      <View style={styles.header}>
         <Text style={styles.headerTitle}>{title}</Text>
-      </BlurView>
+        <LinearGradient
+          colors={['transparent', '#d3d3d3', 'transparent']}
+          start={[0, 0]}
+          end={[1, 0]}
+          style={styles.gradientLine}
+        />
+      </View>
     </View>
   );
 };
@@ -15,7 +21,7 @@ const Header = ({ title }) => {
 const styles = StyleSheet.create({
   headerContainer: {
     position: 'absolute',
-    top: 30,
+    top: 25,
     left: 0,
     right: 0,
     zIndex: 1,
@@ -25,11 +31,19 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: 'transparent',
+    position: 'relative',
   },
   headerTitle: {
     color: 'black',
     fontSize: 20,
     fontWeight: 'bold',
+  },
+  gradientLine: {
+    position: 'absolute',
+    bottom: 10,
+    left: '25%',
+    right: '25%',
+    height: 2,
   },
 });
 
