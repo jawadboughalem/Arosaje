2aff291bfe84b5c1867a3d9a71839dbc4a970ad4
focus . <yassin.rabhi@proton.me>
2024-06-26 11:34:57 +0200

    maj

--- screens/Login.js
+++ screens/Login.js
@@ -5,164 +5,163 @@ import { MaterialCommunityIcons } from '@expo/vector-icons';
 const { IPV4 } = require('../Backend//config/config');
 
 export default function Login({ navigation, setIsLoggedIn }) {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [errors, setErrors] = useState({});
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [errors, setErrors] = useState({});
 
-  const handleLogin = async () => {
-    const validationErrors = {};
+    const handleLogin = async () => {
+        const validationErrors = {};
 
-    if (!email) validationErrors.email = "L'email est requis.";
-    if (!password) validationErrors.password = "Le mot de passe est requis.";
+        if (!email) validationErrors.email = "L'email est requis.";
+        if (!password) validationErrors.password = "Le mot de passe est requis.";
 
-    setErrors(validationErrors);
+        setErrors(validationErrors);
 
-    if (Object.keys(validationErrors).length === 0) {
-      try {
-        const response = await fetch(`http://${IPV4}:3000/login`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ email, password }),
-        });
+        if (Object.keys(validationErrors).length === 0) {
+            try {
+                const response = await fetch(`http://${IPV4}:3000/auth/login`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ email, password }),
+                });
 
-        const data = await response.json();
+                const data = await response.json();
 
-        if (response.ok) {
-          await AsyncStorage.setItem('token', data.token); // Sauvegarde du token dans AsyncStorage
-          setIsLoggedIn(true); // Met à jour l'état global d'authentification
-          Alert.alert("Connexion réussie", "Vous êtes maintenant connecté.");
-          navigation.navigate('Main'); // Navigue vers 'Main' après une connexion réussie
-        } else {
-          Alert.alert("Erreur de connexion", data.error || "Email ou mot de passe incorrect.");
+                if (response.ok) {
+                    setIsLoggedIn(true); // Met à jour l'état global d'authentification
+                    Alert.alert("Connexion réussie", "Vous êtes maintenant connecté.");
+                    navigation.navigate('Main'); // Navigue vers 'Main' après une connexion réussie
+                } else {
+                    Alert.alert("Erreur de connexion", data.error || "Email ou mot de passe incorrect.");
+                }
+                
+            } catch (error) {
+                console.error('Network error:', error);
+                Alert.alert("Erreur de connexion", "Une erreur s'est produite. Veuillez réessayer.");
+            }
         }
-        
-      } catch (error) {
-        console.error('Network error:', error);
-        Alert.alert("Erreur de connexion", "Une erreur s'est produite. Veuillez réessayer.");
-      }
-    }
-  };
+    };
 
-  return (
-    <KeyboardAvoidingView style={{ flex: 1 }} behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
-      <ScrollView contentContainerStyle={styles.scrollContainer}>
-        <View style={styles.container}>
-          <View style={styles.header}>
-            <Text style={styles.title}>A’rosa-je</Text>
-            <MaterialCommunityIcons name="flower" size={100} color="black" style={styles.icon} />
-          </View>
-          <View style={styles.formContainer}>
-            <Text style={styles.formTitle}>Connexion</Text>
-            <TextInput
-              style={styles.input}
-              placeholder="Email"
-              value={email}
-              onChangeText={setEmail}
-              keyboardType="email-address"
-              autoCapitalize="none"
-            />
-            {errors.email && <Text style={styles.errorText}>{errors.email}</Text>}
-            <TextInput
-              style={styles.input}
-              placeholder="Mot de passe"
-              value={password}
-              onChangeText={setPassword}
-              secureTextEntry
-              autoCapitalize="none"
-            />
-            {errors.password && <Text style={styles.errorText}>{errors.password}</Text>}
-            <TouchableOpacity style={styles.button} onPress={handleLogin}>
-              <Text style={styles.buttonText}>Connexion</Text>
-            </TouchableOpacity>
-            <Text style={styles.switchText}>
-              Vous n’avez pas de compte?{' '}
-              <Text style={styles.switchLink} onPress={() => navigation.navigate('Sign')}>
-                Inscription
-              </Text>
-            </Text>
-          </View>
-        </View>
-      </ScrollView>
-    </KeyboardAvoidingView>
-  );
+    return (
+        <KeyboardAvoidingView style={{ flex: 1 }} behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+            <ScrollView contentContainerStyle={styles.scrollContainer}>
+                <View style={styles.container}>
+                    <View style={styles.header}>
+                        <Text style={styles.title}>A’rosa-je</Text>
+                        <MaterialCommunityIcons name="flower" size={100} color="black" style={styles.icon} />
+                    </View>
+                    <View style={styles.formContainer}>
+                        <Text style={styles.formTitle}>Connexion</Text>
+                        <TextInput
+                            style={styles.input}
+                            placeholder="Email"
+                            value={email}
+                            onChangeText={setEmail}
+                            keyboardType="email-address"
+                            autoCapitalize="none"
+                        />
+                        {errors.email && <Text style={styles.errorText}>{errors.email}</Text>}
+                        <TextInput
+                            style={styles.input}
+                            placeholder="Mot de passe"
+                            value={password}
+                            onChangeText={setPassword}
+                            secureTextEntry
+                            autoCapitalize="none"
+                        />
+                        {errors.password && <Text style={styles.errorText}>{errors.password}</Text>}
+                        <TouchableOpacity style={styles.button} onPress={handleLogin}>
+                            <Text style={styles.buttonText}>Connexion</Text>
+                        </TouchableOpacity>
+                        <Text style={styles.switchText}>
+                            Vous n’avez pas de compte?{' '}
+                            <Text style={styles.switchLink} onPress={() => navigation.navigate('Sign')}>
+                                Inscription
+                            </Text>
+                        </Text>
+                    </View>
+                </View>
+            </ScrollView>
+        </KeyboardAvoidingView>
+    );
 }
 
 const styles = StyleSheet.create({
-  scrollContainer: {
-    flexGrow: 1,
-    justifyContent: 'center',
-    backgroundColor: '#5DB075',
-  },
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  header: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 80,
-  },
-  title: {
-    fontSize: 30,
-    fontWeight: 'bold',
-    color: 'black',
-    fontFamily: Platform.select({ ios: 'Helvetica', android: 'sans-serif-light' }),
-  },
-  icon: {
-    marginVertical: 10,
-  },
-  formContainer: {
-    width: '100%',
-    height: '60%',
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderTopLeftRadius: 50,
-    borderTopRightRadius: 50,
-    marginTop: 0,
-    padding: 20,
-  },
-  formTitle: {
-    fontSize: 25,
-    fontWeight: 'bold',
-    color: 'black',
-    marginBottom: 10,
-  },
-  input: {
-    width: '80%',
-    padding: 10,
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 5,
-    marginBottom: 10,
-  },
-  errorText: {
-    color: 'red',
-    marginBottom: 2,
-    alignSelf: 'flex-start',
-    marginLeft: '10%',
-  },
-  button: {
-    backgroundColor: '#5DB075',
-    padding: 10,
-    borderRadius: 30,
-    width: '80%',
-    alignItems: 'center',
-  },
-  buttonText: {
-    color: 'white',
-    fontSize: 19,
-    fontWeight: 'bold',
-  },
-  switchText: {
-    marginTop: 50,
-    color: 'black',
-  },
-  switchLink: {
-    color: '#5DB075',
-    fontWeight: 'bold',
-  },
+    scrollContainer: {
+        flexGrow: 1,
+        justifyContent: 'center',
+        backgroundColor: '#5DB075',
+    },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    header: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 80,
+    },
+    title: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: 'black',
+        fontFamily: Platform.select({ ios: 'Helvetica', android: 'sans-serif-light' }),
+    },
+    icon: {
+        marginVertical: 10,
+    },
+    formContainer: {
+        width: '100%',
+        height: '60%',
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderTopLeftRadius: 50,
+        borderTopRightRadius: 50,
+        marginTop: 0,
+        padding: 20,
+    },
+    formTitle: {
+        fontSize: 25,
+        fontWeight: 'bold',
+        color: 'black',
+        marginBottom: 10,
+    },
+    input: {
+        width: '80%',
+        padding: 10,
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: 5,
+        marginBottom: 10,
+    },
+    errorText: {
+        color: 'red',
+        marginBottom: 2,
+        alignSelf: 'flex-start',
+        marginLeft: '10%',
+    },
+    button: {
+        backgroundColor: '#5DB075',
+        padding: 10,
+        borderRadius: 30,
+        width: '80%',
+        alignItems: 'center',
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 19,
+        fontWeight: 'bold',
+    },
+    switchText: {
+        marginTop: 50,
+        color: 'black',
+    },
+    switchLink: {
+        color: '#5DB075',
+        fontWeight: 'bold',
+    },
 });
