7f3c22613a9576851f708a825e3ce96caea0f045
Jawad BOUGHALEM <jboughalem@dailybiz.com>
2024-06-26 15:53:36 +0200

    MAJ

--- App.js
+++ App.js
@@ -72,7 +72,7 @@ export default function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
-    setIsLoggedIn(false);
+    setIsLoggedIn(true);
   }, []);
 
   return (
