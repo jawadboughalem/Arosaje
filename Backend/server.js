e8daabc07a132389ecf709f602f6100583095689
focus . <yassin.rabhi@proton.me>
2024-06-27 07:01:39 +0200

    maj

--- Backend/server.js
+++ Backend/server.js
@@ -26,26 +26,7 @@ app.get('/protected', authenticateToken, (req, res) => {
 });
 
 // Route protégée pour récupérer les informations de l'utilisateur
-app.get('/user-info', authenticateToken, (req, res) => {
-    const userId = req.userId;
-    console.log(`Fetching info for user ID: ${userId}`);
 
-    const query = `SELECT nom, prenom FROM utilisateurs WHERE Code_Utilisateurs = ?`;
-    db.get(query, [userId], (err, row) => {
-        if (err) {
-            console.error('Database error:', err.message);
-            return res.status(500).json({ error: 'Erreur lors de la récupération des informations utilisateur' });
-        }
-
-        if (!row) {
-            console.warn('No user found for userId:', userId);
-            return res.status(404).json({ error: 'Utilisateur non trouvé' });
-        }
-
-        console.log('User info retrieved:', row);
-        res.status(200).json({ nom: row.nom, prenom: row.prenom });
-    });
-});
 
 app.listen(port, '0.0.0.0', () => {
     console.log(`Serveur démarré sur le port ${port}`);
