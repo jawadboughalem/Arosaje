e1a7a93d1abf8ac0d23c152e896fec3a0b13831f
Nass269 <n.ali90@outlook.com>
2024-07-17 16:24:56 +0200

    Environnement test

---
+++ __test__/navigation/MainNavigator.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import MainNavigator from '../../MainNavigator';
+
+describe('un test', () => {
+    test('test index 1', () => {
+        expect().toBe("")
+    });
+});
